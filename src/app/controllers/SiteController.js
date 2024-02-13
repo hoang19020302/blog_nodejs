7dd695b5281a6897cbb6727ed77258e87b81fe0a
hoang19020302 <tranichhoang2001@gmail.com>
2024-02-13 10:21:12 +0700

    Them router va controller

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,20 @@
+
+class SiteController {
+
+    // [GET] /
+    index(req, res) {
+        res.render('home')
+    }
+
+    //[GET] /search
+    search(req, res) {
+        res.render('search')
+    }
+
+    //[POST] /news
+    home(req, res) {
+        res.send('Hello world!!!')
+    }
+}
+
+module.exports = new SiteController
---
+++ src/routes/news.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+const newsController = require('../app/controllers/NewsController')
+
+//router.use('/:slug/:slug', newsController.show1)
+router.use('/:slug', newsController.show)
+router.use('/', newsController.index)
+
+
+module.exports = router
