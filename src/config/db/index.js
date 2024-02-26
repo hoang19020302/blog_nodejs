be2035e40be5248825ea9d81d1afe06040cda92b
hoang19020302 <tranichhoang2001@gmail.com>
2024-02-26 10:38:53 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+
+mongoose.set("strictQuery", false);
 async function connect() {
     try {
         await mongoose.connect('mongodb://172.23.176.1:27017/f8_education_dev', {
+          
         });
         console.log('Connect successfully!!!');
     } catch (error) {
